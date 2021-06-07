c7336617e4c64735733ca5402ce48613c2fea7f2
Aniket Kumar Pandey <aniketpandey353@gmail.com>
2021-06-08 00:16:14 +0530

    modified internal classes

--- Program.java
+++ Program.java
@@ -1,10 +1,54 @@
-abstract class Car{
-    abstract void accelerate();
-}
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-class Suzuki extends Car{
-    void accelerate()
-    {
+class Program
+{
+   private ArrayList<Integer> data;
+   private String sortAlgo;
+   
+
+   Program()
+   {
+       data=new ArrayList<Integer>();
+       sortAlgo="BubbleSort";
+   }
+   
+   public void InsertValue(int d)
+   {
+       data.add(d);
+   }
+   public void setSortAlgo(String sortAlgo)
+   {
+   
+       this.sortAlgo=sortAlgo;
+   }
+
+   public List<Integer> getSortedList()
+   {
+       Collections.sort(data);
+       return data;
+   }
+
+
+
+    public static void main(String[] args) {
         
+        Program p=new Program();
+        p.setSortAlgo("mergerSort");
+        p.InsertValue(5);
+        p.InsertValue(2);
+        p.InsertValue(4);
+        p.InsertValue(3);
+        p.InsertValue(1);
+
+        List<Integer> result=p.getSortedList();
+        for (Integer i : result) {
+            System.out.println(i);
+        }
+
+
+
     }
 }
